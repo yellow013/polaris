bea1c47162be0e32f76f64e2348a4caba1c0551d
yellow013 <wk9988@gmail.com>
2018-07-05 18:29:32 +0800

    update 2018-07-05

--- indicator/src/main/java/io/ffreedom/indicators/impl/candle/CandleChart.java
+++ indicator/src/main/java/io/ffreedom/indicators/impl/candle/CandleChart.java
@@ -100,7 +100,7 @@ public class CandleChart implements Indicator<Candle> {
 
 	@Override
 	public void endPoint(Candle candle) {
-		endPointCallback.onEvent(candle);
+		endPointCallback.accept(candle);
 	}
 
 }
