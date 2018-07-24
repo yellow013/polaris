8204e0f66add1382c9f9009d583d9f273345d820
Yellow013 <wk9988@gmail.com>
2018-07-24 21:43:05 +0800

    update 2018-07-24

--- financial-market/src/test/java/io/ffreedom/market/data/TradingPeriodTest.java
+++ financial-market/src/test/java/io/ffreedom/market/data/TradingPeriodTest.java
@@ -1,14 +1,12 @@
 package io.ffreedom.market.data;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TradingPeriodTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
