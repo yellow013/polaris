e0569d702104ddb790200b1aacb791726ab4b4ce
Yellow013 <wk9988@gmail.com>
2019-02-05 23:26:40 +0800

    update pools

--- src/main/java/io/ffreedom/indicators/api/IndicatorPeriod.java
+++ src/main/java/io/ffreedom/indicators/api/IndicatorPeriod.java
@@ -5,13 +5,25 @@ import java.time.LocalTime;
 
 public enum IndicatorPeriod {
 
+	S1(Duration.ofSeconds(1)),
+
+	S2(Duration.ofSeconds(2)),
+
 	S5(Duration.ofSeconds(5)),
+
+	S10(Duration.ofSeconds(10)),
 
 	S15(Duration.ofSeconds(15)),
 
+	S30(Duration.ofSeconds(30)),
+
 	M1(Duration.ofMinutes(1)),
 
+	M2(Duration.ofMinutes(2)),
+
 	M5(Duration.ofMinutes(5)),
+
+	M10(Duration.ofMinutes(10)),
 
 	M15(Duration.ofMinutes(15)),
 
@@ -41,8 +53,8 @@ public enum IndicatorPeriod {
 
 	public static void main(String[] args) {
 		for (IndicatorPeriod period : IndicatorPeriod.values()) {
-			System.out.println(period.getDuration().getSeconds());
-			LocalTime plusSeconds = LocalTime.of(21, 0, 0).plusSeconds(period.getDuration().getSeconds());
+			System.out.println(period.getSeconds());
+			LocalTime plusSeconds = LocalTime.of(21, 0, 0).plusSeconds(period.getSeconds());
 			System.out.println(plusSeconds);
 		}
 
