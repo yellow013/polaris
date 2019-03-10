df05156624bdc940f4737e7b0360597097ed528d
yellow013 <wk9988@gmail.com>
2019-03-10 23:16:54 +0800

    add bar impl

---
+++ src/main/java/io/ffreedom/polaris/indicators/impl/Bar.java
@@ -0,0 +1,15 @@
+package io.ffreedom.polaris.indicators.impl;
+
+public final class Bar {
+	
+	public static void main(String[] args) {
+		
+		int i = 20501227;
+		int j = 235959999;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(Integer.MAX_VALUE);
+		
+	}
+
+}
