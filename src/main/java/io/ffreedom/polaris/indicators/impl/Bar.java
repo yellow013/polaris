2b68a01c9828e658b53e7a1bbf72087cf4054ee9
yellow013 <wk9988@gmail.com>
2019-03-11 23:34:42 +0800

    update Bar impl

--- src/main/java/io/ffreedom/polaris/indicators/impl/Bar.java
+++ src/main/java/io/ffreedom/polaris/indicators/impl/Bar.java
@@ -1,15 +1,18 @@
 package io.ffreedom.polaris.indicators.impl;
 
+import javax.annotation.concurrent.NotThreadSafe;
+
+import io.ffreedom.common.datetime.TimePoint;
+
+@NotThreadSafe
 public final class Bar {
-	
-	public static void main(String[] args) {
-		
-		int i = 20501227;
-		int j = 235959999;
-		System.out.println(i);
-		System.out.println(j);
-		System.out.println(Integer.MAX_VALUE);
-		
-	}
+
+	private TimePoint startTimePoint;
+	private TimePoint endTimePoint;
+	private double[] priceRecord;
+	private double startPrice = 0.0D;
+	private double endPrice = 0.0D;
+	private double highest = Double.MIN_VALUE;
+	private double lowest = Double.MAX_VALUE;
 
 }
