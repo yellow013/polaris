c34d6567c4a13b9690377166066c77fe0a4481b9
yellow013 <wk9988@gmail.com>
2019-04-05 12:30:02 +0800

    update pools

--- src/main/java/io/ffreedom/polaris/indicators/pools/TradingPeriodPool.java
+++ src/main/java/io/ffreedom/polaris/indicators/pools/TradingPeriodPool.java
@@ -1,5 +1,7 @@
 package io.ffreedom.polaris.indicators.pools;
 
+import java.time.LocalTime;
+
 import javax.annotation.concurrent.ThreadSafe;
 
 import org.eclipse.collections.api.map.primitive.ImmutableIntObjectMap;
@@ -10,6 +12,7 @@ import io.ffreedom.common.collect.ECollections;
 import io.ffreedom.polaris.datetime.TradingPeriod;
 import io.ffreedom.polaris.financial.Instrument;
 import io.ffreedom.polaris.financial.Symbol;
+import io.ffreedom.polaris.financial.futures.ChinaFuturesSymbol;
 
 @ThreadSafe
 public final class TradingPeriodPool {
@@ -19,11 +22,11 @@ public final class TradingPeriodPool {
 	private TradingPeriodPool() {
 	}
 
-	// Map<IndicatorPeriod, Map<Symbol, Set<TimePeriod>>>
+	// Map<Symbol, Set<TimePeriod>>
 	private MutableIntObjectMap<ImmutableSortedSet<TradingPeriod>> tradingPeriodMap = ECollections
 			.newIntObjectHashMap();
 
-	// Map<IndicatorPeriod, Map<Symbol, Map<SerialNumber, TimePeriod>>>
+	// Map<Symbol, Set<TimePeriod>>
 	private ImmutableIntObjectMap<ImmutableSortedSet<TradingPeriod>> immutablePool;
 
 	public void register(Symbol... symbols) {
@@ -56,6 +59,34 @@ public final class TradingPeriodPool {
 
 	public ImmutableSortedSet<TradingPeriod> getTradingPeriodSet(Symbol symbol) {
 		return immutablePool.get(symbol.getSymbolId());
+	}
+
+	public TradingPeriod getAfterTradingPeriod(Instrument instrument, LocalTime time) {
+		return getAfterTradingPeriod(instrument.getSymbol(), time);
+	}
+
+	public TradingPeriod getAfterTradingPeriod(Symbol symbol, LocalTime time) {
+		ImmutableSortedSet<TradingPeriod> tradingPeriodSet = getTradingPeriodSet(symbol);
+		TradingPeriod rtnTradingPeriod = null;
+		int baseTime = time.toSecondOfDay();
+		int baseDiff = Integer.MAX_VALUE;
+		for(TradingPeriod tradingPeriod : tradingPeriodSet) {
+			int startSecondOfDay = tradingPeriod.getStartSecondOfDay();
+			int diff = Math.abs(startSecondOfDay - baseTime);
+			if(diff < baseDiff) {
+				baseDiff = diff;
+				rtnTradingPeriod = tradingPeriod;
+			}
+		}
+		return rtnTradingPeriod;
+	}
+	
+	public static void main(String[] args) {
+		
+		Singleton.register(ChinaFuturesSymbol.values());
+		
+		TradingPeriod afterTradingPeriod = Singleton.getAfterTradingPeriod(ChinaFuturesSymbol.RB, LocalTime.now());
+		System.out.println(afterTradingPeriod.getStartTime());
 	}
 
 }
