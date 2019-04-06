0d14d5b9b40f748ad96cf44f5f9acef43baf890c
yellow013 <wk9988@gmail.com>
2019-04-07 00:18:42 +0800

    update bar impl

--- src/main/java/io/ffreedom/polaris/indicators/impl/bar/point/VolumeBar.java
+++ src/main/java/io/ffreedom/polaris/indicators/impl/bar/point/VolumeBar.java
@@ -1,5 +1,46 @@
-package io.ffreedom.polaris.indicators.impl.bar;
+package io.ffreedom.polaris.indicators.impl.bar.point;
 
-public class VolumeBar {
+import io.ffreedom.polaris.datetime.TimeStarted;
+import io.ffreedom.polaris.indicators.api.IndicatorPeriod;
+import io.ffreedom.polaris.indicators.api.Point;
+import io.ffreedom.polaris.market.BasicMarketData;
+
+public class VolumeBar implements Point<TimeStarted, VolumeBar> {
+
+	@Override
+	public IndicatorPeriod getPeriod() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public TimeStarted getXAxis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VolumeBar getYAxis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VolumeBar generateNext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onMarketData(BasicMarketData marketData) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
