de8486dfe29e7db404b61b9205359bf8694ab715
yellow013 <wk9988@gmail.com>
2019-04-08 23:56:04 +0800

    update macd

--- src/main/java/io/ffreedom/polaris/indicators/impl/macd/point/MacdPoint.java
+++ src/main/java/io/ffreedom/polaris/indicators/impl/macd/point/MacdPoint.java
@@ -1,14 +1,15 @@
 package io.ffreedom.polaris.indicators.impl.macd.point;
 
 import io.ffreedom.polaris.datetime.TimePeriod;
+import io.ffreedom.polaris.financial.Instrument;
 import io.ffreedom.polaris.indicators.api.IndicatorPeriod;
 import io.ffreedom.polaris.indicators.impl.TimePeriodPoint;
 import io.ffreedom.polaris.market.BasicMarketData;
 
 public class MacdPoint extends TimePeriodPoint<MacdPoint> {
 
-	private MacdPoint(int index, IndicatorPeriod period, TimePeriod timePeriod) {
-		super(index, period, timePeriod);
+	private MacdPoint(int index, Instrument instrument, IndicatorPeriod period, TimePeriod timePeriod) {
+		super(index, instrument, period, timePeriod);
 	}
 
 	@SuppressWarnings("unused")
