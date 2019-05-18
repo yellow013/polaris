2a500b3a4d968e0a287666e93e8739ce8cc39e41
yellow013 <wk9988@gmail.com>
2019-05-18 23:56:16 +0800

    update time pool

--- src/main/java/io/ffreedom/polaris/indicators/pools/TimePeriodPool.java
+++ src/main/java/io/ffreedom/polaris/indicators/pools/TimePeriodPool.java
@@ -3,15 +3,14 @@ package io.ffreedom.polaris.indicators.pools;
 import javax.annotation.concurrent.ThreadSafe;
 
 import org.eclipse.collections.api.list.MutableList;
-import org.eclipse.collections.api.map.primitive.ImmutableIntObjectMap;
 import org.eclipse.collections.api.map.primitive.ImmutableLongObjectMap;
-import org.eclipse.collections.api.map.primitive.MutableIntObjectMap;
 import org.eclipse.collections.api.map.primitive.MutableLongObjectMap;
 import org.eclipse.collections.api.set.sorted.ImmutableSortedSet;
 import org.eclipse.collections.api.set.sorted.MutableSortedSet;
 
 import io.ffreedom.common.collect.MutableMaps;
 import io.ffreedom.common.collect.MutableSets;
+import io.ffreedom.common.utils.JointIdUtil;
 import io.ffreedom.polaris.datetime.TimePeriod;
 import io.ffreedom.polaris.datetime.TradingPeriod;
 import io.ffreedom.polaris.financial.Instrument;
@@ -27,30 +26,38 @@ public final class TimePeriodPool {
 	}
 
 	/**
+	 * 使用联合主键进行索引,高位为period, 低位为symbolId <br>
 	 * 可变的Pool,最终元素为Set <br>
-	 * Map<IndicatorPeriod, Map<Symbol, Set<TimePeriod>>>
+	 * Map<(period + symbolId), Set<TimePeriod>>
 	 */
-	private MutableLongObjectMap<MutableIntObjectMap<ImmutableSortedSet<TimePeriod>>> timePeriodSetsPool = MutableMaps
+	private MutableLongObjectMap<ImmutableSortedSet<TimePeriod>> timePeriodSetsPool = MutableMaps
 			.newLongObjectHashMap();
 
 	/**
+	 * 使用联合主键进行索引,高位为period, 低位为symbolId <br>
 	 * 可变的Pool,最终元素为Map <br>
-	 * Map<IndicatorPeriod, Map<Symbol, Map<SerialNumber,TimePeriod>>>
+	 * Map<(period + symbolId), Map<SerialNumber,TimePeriod>>
 	 */
-	private MutableLongObjectMap<MutableIntObjectMap<ImmutableLongObjectMap<TimePeriod>>> timePeriodMapsPool = MutableMaps
+	private MutableLongObjectMap<ImmutableLongObjectMap<TimePeriod>> timePeriodMapsPool = MutableMaps
 			.newLongObjectHashMap();
 
 	/**
+	 * 使用联合主键进行索引,高位为period, 低位为symbolId <br>
 	 * 不可变的Pool,用于查询,最终元素为Set<br>
-	 * Map<IndicatorPeriod, Map<Symbol, Set<TimePeriod>>>
+	 * Map<(period + symbolId), Set<TimePeriod>>
 	 */
-	private ImmutableLongObjectMap<ImmutableIntObjectMap<ImmutableSortedSet<TimePeriod>>> immutableTimePeriodSetsPool;
+	private ImmutableLongObjectMap<ImmutableSortedSet<TimePeriod>> immutableTimePeriodSetsPool;
 
 	/**
+	 * 使用联合主键进行索引,高位为period, 低位为symbolId <br>
 	 * 不可变的Pool,用于查询,最终元素为Map <br>
-	 * Map<IndicatorPeriod, Map<Symbol, Map<SerialNumber, TimePeriod>>>
+	 * Map<(period + symbolId), Map<SerialNumber, TimePeriod>>
 	 */
-	private ImmutableLongObjectMap<ImmutableIntObjectMap<ImmutableLongObjectMap<TimePeriod>>> immutableTimePeriodMapsPool;
+	private ImmutableLongObjectMap<ImmutableLongObjectMap<TimePeriod>> immutableTimePeriodMapsPool;
+
+	public void register(Symbol symbol, IndicatorTimePeriod... periods) {
+		register(new Symbol[] { symbol }, periods);
+	}
 
 	public void register(Symbol[] symbols, IndicatorTimePeriod... periods) {
 		if (symbols == null)
@@ -58,57 +65,31 @@ public final class TimePeriodPool {
 		if (periods == null)
 			throw new IllegalArgumentException("Illegal Argument -> periods in null");
 		for (IndicatorTimePeriod period : periods)
-			generateTimePeriod(symbols, period);
+			generateTimePeriod(period, symbols);
 		toImmutable();
 	}
 
-	private void generateTimePeriod(Symbol[] symbols, IndicatorTimePeriod period) {
-		MutableIntObjectMap<ImmutableSortedSet<TimePeriod>> symbolSets = timePeriodSetsPool.get(period.getSeconds());
-		MutableIntObjectMap<ImmutableLongObjectMap<TimePeriod>> symbolMaps = timePeriodMapsPool
-				.get(period.getSeconds());
-		if (symbolSets == null) {
-			symbolSets = MutableMaps.newIntObjectHashMap();
-			timePeriodSetsPool.put(period.getSeconds(), symbolSets);
-		}
-		if (symbolMaps == null) {
-			symbolMaps = MutableMaps.newIntObjectHashMap();
-			timePeriodMapsPool.put(period.getSeconds(), symbolMaps);
-		}
+	private void generateTimePeriod(IndicatorTimePeriod period, Symbol[] symbols) {
 		for (Symbol symbol : symbols) {
 			MutableSortedSet<TimePeriod> timePeriodSet = MutableSets.newTreeSortedSet();
 			MutableLongObjectMap<TimePeriod> timePeriodMap = MutableMaps.newLongObjectHashMap();
 			// 获取指定品种下的全部交易时段,将交易时段按照指定指标周期切分
 			symbol.getTradingPeriodSet().forEach(tradingPeriod -> {
 				MutableList<TimePeriod> segmentByDuration = tradingPeriod.segmentByDuration(period.getDuration());
-				timePeriodSet.addAll(segmentByDuration);
 				segmentByDuration.each(timePeriod -> {
+					timePeriodSet.add(timePeriod);
 					timePeriodMap.put(timePeriod.getSerialNumber(), timePeriod);
 				});
 			});
-			symbolSets.put(symbol.getSymbolId(), timePeriodSet.toImmutable());
-			symbolMaps.put(symbol.getSymbolId(), timePeriodMap.toImmutable());
+			long jointId = JointIdUtil.jointId((int) period.getSeconds(), symbol.getSymbolId());
+			timePeriodSetsPool.put(jointId, timePeriodSet.toImmutable());
+			timePeriodMapsPool.put(jointId, timePeriodMap.toImmutable());
 		}
 	}
 
 	public void toImmutable() {
-		// 创建临时的可变结构,内部包含不可变的结构.
-		MutableLongObjectMap<ImmutableIntObjectMap<ImmutableSortedSet<TimePeriod>>> tempSetsPool = MutableMaps
-				.newLongObjectHashMap();
-		// 将可变Pool中的元素转移到临时池中,将元素转换为不可变
-		timePeriodSetsPool.forEachKeyValue(
-				(long period, MutableIntObjectMap<ImmutableSortedSet<TimePeriod>> value) -> tempSetsPool.put(period,
-						value.toImmutable()));
-		// 将临时可变次转换为最终查询使用的不可变池
-		this.immutableTimePeriodSetsPool = tempSetsPool.toImmutable();
-
-		// 创建临时的可变结构,内部包含不可变的结构.
-		MutableLongObjectMap<ImmutableIntObjectMap<ImmutableLongObjectMap<TimePeriod>>> tempMapsPool = MutableMaps
-				.newLongObjectHashMap();
-		// 将可变Pool中的元素转移到临时池中,将元素转换为不可变
-		timePeriodMapsPool.forEachKeyValue(
-				(long period, MutableIntObjectMap<ImmutableLongObjectMap<TimePeriod>> value) -> tempMapsPool.put(period,
-						value.toImmutable()));
-		this.immutableTimePeriodMapsPool = tempMapsPool.toImmutable();
+		this.immutableTimePeriodSetsPool = timePeriodSetsPool.toImmutable();
+		this.immutableTimePeriodMapsPool = timePeriodMapsPool.toImmutable();
 	}
 
 	/**
@@ -131,7 +112,8 @@ public final class TimePeriodPool {
 	 * @return
 	 */
 	public ImmutableSortedSet<TimePeriod> getTimePeriodSet(IndicatorTimePeriod period, Symbol symbol) {
-		return immutableTimePeriodSetsPool.get(period.getSeconds()).get(symbol.getSymbolId());
+		long jointId = JointIdUtil.jointId((int) period.getSeconds(), symbol.getSymbolId());
+		return immutableTimePeriodSetsPool.get(jointId);
 	}
 
 	public TradingPeriod getNextTimePeriod(Instrument instrument, IndicatorTimePeriod period, TimePeriod timePeriod) {
@@ -139,8 +121,8 @@ public final class TimePeriodPool {
 	}
 
 	public TradingPeriod getNextTimePeriod(Symbol symbol, IndicatorTimePeriod period, TimePeriod timePeriod) {
-		ImmutableLongObjectMap<TimePeriod> immutableLongObjectMap = immutableTimePeriodMapsPool.get(period.getSeconds())
-				.get(symbol.getSymbolId());
+		long jointId = JointIdUtil.jointId((int) period.getSeconds(), symbol.getSymbolId());
+		immutableTimePeriodMapsPool.get(jointId);
 		// TODO
 		return null;
 	}
